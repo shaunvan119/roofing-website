1f3adb2e9f6bfcb76b8271a7eb8ca6d8302d5155
shaunvan119 <shaunvan@live.com.au>
2022-10-25 10:31:10 +1100

    first commit and file set up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./containers/Header";
+import Main from "./containers/Main";
+import Footer from "./containers/Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Main/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
