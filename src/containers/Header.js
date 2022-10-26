66873ed129cc667a371842491c7c3d289f32a131
shaunvan119 <shaunvan@live.com.au>
2022-10-27 10:24:34 +1100

    header completed

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./header.css"
+import logoWhite from "../assets/roofingWhite.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="top_header">
+    <img className="logo" src={logoWhite} alt="logo"/>
+    <button  className="contact_button"><a href="tel:0433310964">Call 0433310964</a></button>
+ 
+</div>
   )
 }
 
