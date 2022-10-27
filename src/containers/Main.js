f05acfb1e9279ae570458bc1de853657eeec5613
shaunvan119 <shaunvan@live.com.au>
2022-10-27 11:18:45 +1100

    contact form component

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./main.css"
+import backgroundVid from "../assets/backgroundVid.mp4"
+import ContactForm from '../components/ContactFrom'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className="main">
+      <div className='overlay'></div>
+      <video src={backgroundVid} autoPlay loop muted/>
+      <div className="content">
+      <div className="free_quote_form">
+      
+      <ContactForm/>
+
+      </div>
+        
+      </div>
+    </div>
   )
 }
 
