5e03eafe43488a45c3b9506e1edecb308e9cccf5
shaunvan119 <shaunvan@live.com.au>
2022-10-28 14:09:01 +1100

    completed botton main container and grid section

--- src/components/GridPics.js
+++ src/components/GridPics.js
@@ -14,37 +14,37 @@ export default function GridPics() {
         <div class="container">
             <img className="grid-image colorbond_image" src={colorbondAusRoof} alt="colorbond roof" />
             <div class="overlay">
-            <div class="text">We love being part of a online fit community</div>
+            <div class="text">Colorbond, metal roofing and re-roofing.</div>
         </div>
   </div>
         <div class="container">
             <img className="grid-image grid-image-right" src={TiledAusRoof} alt="tiled roof" />
             <div class="overlay overlay-right">
-            <div class="text">Online support pushed me to the next level</div>
+            <div class="text">Tile roofing and restoration</div>
             </div>
     </div>
         <div class="container">
         <img className="grid-image grid-image-left" src={AusRoof} alt="house" />
         <div class="overlay overlay-left">
-        <div class="text">The online diet plans gave me structure</div>
+        <div class="text">General roof repairs and tile to metal roof conversions</div>
         </div>
     </div>
         <div class="container">
         <img className="grid-image " src={newBuild} alt="new house" />
         <div class="overlay overlay-right-two">
-        <div class="text">I enjoy the online community chat for tips</div>
+        <div class="text">Roofing solutions for new and old homes</div>
         </div>
     </div>
         <div class="container">
         <img className="grid-image grid-image-left" src={metalRoof} alt="metal roof" />
         <div class="overlay overlay-left-two">
-        <div class="text">We love the gym based online team workouts</div>
+        <div class="text">Quality of build and service is 100%</div>
         </div>
     </div>
         <div class="container">
         <img className="grid-image" src={roofClean} alt="before after clean roof" />
         <div class="overlay overlay-right-three">
-        <div class="text">The online programs are easy to follow</div>
+        <div class="text">Roof cleaning and painting</div>
         </div>
     </div>
     </div>
